9f27258fcd3811f3b6ceab1d62a7e1e30cad6b1d
Tan Qiangqiang <qiang.tan@moonx.ai>
2019-05-21 17:37:29 +0800

    initial ogl window use glfw & glew.

---
+++ twShading.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+#include <glew/glew.h>
+//include gl after glew
+#include <GL/gl.h>
+#include <glfw/glfw3.h>
+
+int main(int, char **)
+{
+    GLFWwindow *window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(1024, 768, "Twinkle Shading", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        /* Problem: glewInit failed, something is seriously wrong. */
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+    }
+    fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+
+
+    char *vendor = (char *)glGetString( GL_VENDOR );
+	char *renderer = (char *)glGetString( GL_RENDERER );
+	char *version = (char *)glGetString( GL_VERSION );
+
+    std::cout << "Vendor:" << vendor <<std::endl;
+    std::cout << "Render:" << renderer <<std::endl;
+    std::cout << "OGL Version:" << version <<std::endl;
+
+    int attrib_counts;
+    glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &attrib_counts);
+    std::cout << "GL_MAX_VERTEX_ATTRIBS:" << attrib_counts << std::endl;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+        glClearColor(0.5f, 0.7f, 0.8f, 1.0f);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
