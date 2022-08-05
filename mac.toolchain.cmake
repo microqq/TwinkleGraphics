4026109c0376f9e98d437d19d722a9c673f80685
microqq <qiang347347@163.com>
2022-08-05 17:51:00 +0800

    update cmakelists(APPLE).

--- mac.toolchain.cmake
+++ mac.toolchain.cmake
@@ -1,20 +1,8 @@
 # MacOS 
 # brew install glew & glfw & assimp & freeimage
-# cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../mac.toolchain.cmake ..
+# cmake -G "Xcode" ..
 
-cmake_minimum_required( VERSION 2.6.3 )
-SET(CMAKE_SYSTEM_NAME MacOS)
-
-SET (CMAKE_C_COMPILER             "/usr/bin/clang")
-SET (CMAKE_C_FLAGS                "-Wall -std=c11")
-SET (CMAKE_C_FLAGS_DEBUG          "-g- O0")
-SET (CMAKE_C_FLAGS_MINSIZEREL     "-Os -DNDEBUG")
-SET (CMAKE_C_FLAGS_RELEASE        "-O4 -DNDEBUG")
-SET (CMAKE_C_FLAGS_RELWITHDEBINFO "-O2 -g")
-
-SET (CMAKE_CXX_COMPILER      "/usr/bin/clang++" )
-SET (CMAKE_CXX_FLAGS   "-std=c++17")    
-SET (CMAKE_CXX_FLAGS   "-g")
-SET (CMAKE_CXX_FLAGS   "-Wall")
-SET (CMAKE_CXX_FLAGS_DEBUG   "-g -O0" )
-SET (CMAKE_CXX_FLAGS_RELEASE "-O2 -DNDEBUG " )
+# Xcode 
+# 1. Preference->Locations->Custom Path, add GLEW_INCLUDE\GLEW_LIB...
+# 2. choose Build Settings->Search Path(header and library)
+# 3. choose Build Phases->Link Libraries and Libraries Phases->add related libraries
